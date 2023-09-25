8c791687fd2323cc3b2017c04fcbbb92fe68d2f7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 18:02:11 +0300

    Challenge - start

--- 03_useTypeofToSeeVarType.js
+++ 03_useTypeofToSeeVarType.js
@@ -6,3 +6,11 @@ console.log(typeof "");  // string
 console.log(typeof 0);  // number
 console.log(typeof []);  // object
 console.log(typeof {});  // object
+
+/* Add two console.log() statements to check the typeof
+each of the two variables seven and three in the code. */
+
+let seven = 7;
+let three = 3;
+console.log(seven + three);
+// Only change code below this line.
