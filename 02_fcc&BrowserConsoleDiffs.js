cfce7f10810bed226aca734d12ec42420e5e8b7d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 17:36:59 +0300

    Initial commit

---
+++ 02_fcc&BrowserConsoleDiffs.js
@@ -0,0 +1,5 @@
+/* Understanding the Differences between the 
+freeCodeCamp and Browser Console */
+
+/* The freeCodeCamp console will only output log messages,
+while the browser consoe will output all messages. */
