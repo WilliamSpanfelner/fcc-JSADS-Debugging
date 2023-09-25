287a7b334ba8f7ea53222e1b39c1136a20f7eb87
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 18:14:18 +0300

    Challenge - start

--- 04_catchMisspelledNames.js
+++ 04_catchMisspelledNames.js
@@ -1,1 +1,11 @@
 // Catch Misselled Variable and Function Names
+/* JavaScript variable and function names are case sensitive.
+Misspellings are a common source of reference errors. */
+
+/* Fix the two spelling errors in the code so the 
+netWorkingCapital calculation works. */
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
