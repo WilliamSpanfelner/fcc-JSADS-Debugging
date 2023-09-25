dd522575b7afba1b968e957b57a1a8a2b32e7780
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 20:01:12 +0300

    Challenge - start

--- 10_catchOffBy1Errors.js
+++ 10_catchOffBy1Errors.js
@@ -13,3 +13,18 @@ for (let j = 1; j < len; j++) {
 for (let k = 0; k < len; k++) {
   console.log(alphabet[k]);
 }
+
+/* Fix the two indexing errors in the following function so all
+the numbers 1 through 5 are printed to the console. */
+
+function countToFive() {
+  let firstFive = '12345';
+  let len = firstFive.length;
+  //Only change code below this line
+  for (let i = 1; i <= len; i++) {
+    // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
