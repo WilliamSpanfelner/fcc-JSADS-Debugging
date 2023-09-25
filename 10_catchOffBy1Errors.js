6cc85f94490bda67743e545bea311408b36f2475
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 19:54:37 +0300

    Add OBOE examples

--- 10_catchOffBy1Errors.js
+++ 10_catchOffBy1Errors.js
@@ -1,3 +1,15 @@
 // Catch Off By One Errors When Using Indexing
 
 // Off By One Errors (OBOE)
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {
+  console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {
+  console.log(alphabet[k]);
+}
