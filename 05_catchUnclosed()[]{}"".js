ce7b5fcc9966a862c48e404950b70d33f48bf147
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 18:29:29 +0300

    Challenge - solution

--- 05_catchUnclosed()[]{}"".js
+++ 05_catchUnclosed()[]{}"".js
@@ -1,8 +1,9 @@
-/* Catch Unclosed Parentheses, Brackets, Braces and Quotes
+// Catch Unclosed Parentheses, Brackets, Braces and Quotes
 
 // Fix the two pair errors in the code
 
-let myArray = [1, 2, 3;
-let arraySum = myArray.reduce(previous, current => previous + current);
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) => 
+(previous + current));
 console.log(`Sum of array values is: ${arraySum}`);
- 
+// Sum of array values is: 6
