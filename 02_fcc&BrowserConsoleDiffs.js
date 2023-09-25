1a374e6e0b3dd9577989b14b9507348aabe9acd4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 17:48:14 +0300

    Challenge - solution

--- 02_fcc&BrowserConsoleDiffs.js
+++ 02_fcc&BrowserConsoleDiffs.js
@@ -15,3 +15,6 @@ View the two consoles to see the log. Finally, use
 console.clear after your log to clear the browser 
 console. View the difference in the two consoles.
 */
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
