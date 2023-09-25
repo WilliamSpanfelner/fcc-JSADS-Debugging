5c69734efbcb9b7f4721c952db5ad182ced7de4b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 18:43:18 +0300

    Match single & double quotes escape where necessry

--- 06_catchMixedQuotes.js
+++ 06_catchMixedQuotes.js
@@ -1,1 +1,9 @@
 // Catch Mixed Usage of Single and Double Quotes
+// Mixing single and double quotes will cause syntax error.
+
+const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";  // ok
+const quoteInString = "Goucho Marx once said 'Quote me as saying I was mis-quoted.'";  // ok
+const uhOhGroucho = 'I've had a perfectly wonderful evening, but this wasn't it';  // not ok
+
+// Escape the quotes inside the string with a backslash
+const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it';  // ok
