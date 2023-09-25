5e9610cd9469c12eec616193d49fcd4ab58eefd0
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 17:59:49 +0300

    Use typeof to log several types

--- 03_useTypeofToSeeVarType.js
+++ 03_useTypeofToSeeVarType.js
@@ -1,1 +1,8 @@
 // Use typeof to Check the Type of a Variable
+/* Ensure variables are of the expected types
+when performing calculations especially */
+
+console.log(typeof "");  // string
+console.log(typeof 0);  // number
+console.log(typeof []);  // object
+console.log(typeof {});  // object
