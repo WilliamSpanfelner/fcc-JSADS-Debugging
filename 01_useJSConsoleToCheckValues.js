be3fa732ce05fe88a667bd30e6f4b599ad5e5ed9
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 17:25:09 +0300

    Use console.log() to display values in console.

--- 01_useJSConsoleToCheckValues.js
+++ 01_useJSConsoleToCheckValues.js
@@ -1,1 +1,5 @@
 // Use the JavaScript Console to Check the Value of a Variable
+/* console.log(x) allows the contents of the variable x to be
+displayed in the console */
+
+console.log("Hello world!");
