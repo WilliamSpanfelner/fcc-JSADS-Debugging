c84371630520f15c00653d38ab54ad87f7159f53
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 20:40:15 +0300

    Challenge - solution

--- 11_carefullyReinitializeLoopVariables.js
+++ 11_carefullyReinitializeLoopVariables.js
@@ -1,1 +1,33 @@
 // Use Caution When Reinitializing Variables Inside a Loop
+
+/* the following function is supposed to create a two-
+dimensional array with m rows and n columns of zeroes.
+Unfortunately, it's not producing the expected output
+because teh row variable isn't being reinitialized (set back
+to an empty array) in the outer loop.  Fix the code so it
+returns a correct 3x2 array of zeroes, which looks like
+[[0, 0], [0, 0], [0, 0]]. */
+
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    
+    for (let i = 0; i < m; i++) {
+        // Adds the m-th row into newArray
+        let row = [];
+        for (let j = 0; j < n; j++) {
+            // Pushes z zeroes into the 
+            // current row to create the columns
+            row.push(0);
+            console.log(row, j);
+        }
+        // Pushes the current row, which 
+        // now has n zeroes in it, to the array
+        newArray.push(row);
+        console.log(newArray, i)
+    }
+    return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
