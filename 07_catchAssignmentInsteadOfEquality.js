e4f2662aea67783d335a64d622fe1b63cdf33f34
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 19:03:17 +0300

    Misuse assingment operator for equivalence

--- 07_catchAssignmentInsteadOfEquality.js
+++ 07_catchAssignmentInsteadOfEquality.js
@@ -1,2 +1,16 @@
 /* Catch Use of Assignment Operator Instead of 
 Equality Operator */
+
+// A conditional will expext a comparison operator
+// falsy values: false, 0, "", NaN, undefined, and null
+
+let x = 1;
+let y = 2;
+if (x = y) {
+    result = "x was just assigned the value of y"
+} else {
+    result = "This else branch is not reachable."
+}
+
+console.log(result, x, y);
+// x was just assigned the value of y 2 2
