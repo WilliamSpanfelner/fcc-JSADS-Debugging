160954990a5d821bfebbced05c2fb5cc040492d7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 18:05:34 +0300

    Challenge - solution

--- 03_useTypeofToSeeVarType.js
+++ 03_useTypeofToSeeVarType.js
@@ -11,6 +11,8 @@ console.log(typeof {});  // object
 each of the two variables seven and three in the code. */
 
 let seven = 7;
-let three = 3;
-console.log(seven + three);
+let three = "3";
+console.log(seven + three);  // 73
 // Only change code below this line.
+console.log(typeof seven);  // number
+console.log(typeof three);  // string
