c3453371f7c3ab991cfe9499349ae208cc07977f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-25 19:35:33 +0300

    Add example of function reference

--- 08_catchMissingParentheses.js
+++ 08_catchMissingParentheses.js
@@ -1,3 +1,14 @@
 /* Catch Missing Open and Closing Parenthesis After
 a Function Call */
- 
+/* Logging variable values to the console or their types 
+and spotting the a variable is set to a function reference 
+is a way of locating a potential error. */
+
+function myFunction() {
+    return "You rock!";
+}
+let varOne = myFunction;
+let varTwo = myFunction();
+
+console.log(varOne, varTwo);
+// [Function: myFunction] You rock!
